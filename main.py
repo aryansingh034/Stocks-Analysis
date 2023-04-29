bda4055100220f801d38af4a2f3df3577916c01b
aryansingh034 <132141859+aryansingh034@users.noreply.github.com>
2023-04-29 19:20:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 print("Select the stock for analysis:")
-print("1.AMZN\n 2.TSLA\n 3.INTC\n 4.AAPL\n 5.F")
+print("1.AMZN\n 2.TSLA\n 3.INTC\n 4.AAPL\n 5.F\n 6.AMD\n 7.META\n 8.NVDA\n 9.GOOGL\n 10.MSFT\n 11.CSCO\n 12.BABA\n 13.WBD\n 14.LYD\n 15.JPM")
 a=int(input("Select the stock for analysis:"))
 if a==1:
     symbol="AMZN"
@@ -12,6 +12,27 @@ elif a==4:
     symbol="AAPL"
 elif a==5:
     symbol="F"
+elif a==6:
+    symbol="AMD"
+elif a==7:
+    symbol="META"
+elif a==8:
+    symbol="NVDA"
+elif a==9:
+    symbol="GOOGL"
+elif a==10:
+    symbol="MSFT"
+elif a==11:
+    symbol="CSCO"
+elif a==12:
+    symbol="BABA"
+elif a==13:
+    symbol="WBD"
+elif a==14:
+    symbol="LYD"
+elif a==15:
+    symbol="JPM"
+
     
 
 # Set the ticker symbol and the start and end dates for the analysis
